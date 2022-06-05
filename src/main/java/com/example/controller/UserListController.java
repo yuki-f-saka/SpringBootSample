fc77edba86ac5b2d95f75447bfac55711822fefc
Yuki Funasaka <yuki.funasaka@uhuru.jp>
2022-06-05 18:05:35 +0900

    ユーザー検索機能追加

--- src/main/java/com/example/controller/UserListController.java
+++ src/main/java/com/example/controller/UserListController.java
@@ -2,14 +2,18 @@ package com.example.controller;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.user.model.MUser;
 import com.example.domain.user.service.UserService;
+import com.example.form.UserListForm;
 
 @Controller
 @RequestMapping("/user")
@@ -18,10 +22,24 @@ public class UserListController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@GetMapping("/list")
-	public String getUserList(Model model) {
-		List<MUser> userList = userService.getUsers();
+	public String getUserList(UserListForm form, Model model) {
+		MUser user = modelMapper.map(form, MUser.class);
+		List<MUser> userList = userService.getUsers(user);
 		model.addAttribute("userList", userList);
 		return "/user/list";
+	}
+	
+//	ユーザー検索処理
+	@PostMapping("/list")
+	public String postUserList(@ModelAttribute UserListForm form, Model model) {
+		MUser user = modelMapper.map(form, MUser.class);
+		
+		List<MUser> userList = userService.getUsers(user);
+		model.addAttribute("userList", userList);
+		return "user/list";
 	}
 }
