07dbab4407e8a47a57ec2cd8e3cab7b47f5b452a
Yuki Funasaka <yuki.funasaka@uhuru.jp>
2022-06-13 00:04:35 +0900

    ユーザー詳細画面に部署名を追加

--- src/main/java/com/example/form/UserDetailForm.java
+++ src/main/java/com/example/form/UserDetailForm.java
@@ -2,6 +2,8 @@ package com.example.form;
 
 import java.util.Date;
 
+import com.example.domain.user.model.Department;
+
 import lombok.Data;
 
 @Data
@@ -12,4 +14,5 @@ public class UserDetailForm {
 	private Date birthday;
 	private Integer age;
 	private Integer gender;
+	private Department department;
 }
